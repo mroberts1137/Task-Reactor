e05f2f7ddcb3eb38c0b2a11ea2fe18e3858396ab
mroberts1137 <91620048+mroberts138@users.noreply.github.com>
2024-04-16 20:27:03 -0400

    post registration form to back end

--- src/components/RegisterForm.js
+++ src/components/RegisterForm.js
@@ -1,9 +1,12 @@
 import { useRef, useState, useEffect } from "react";
 import { faCheck, faTimes, faInfoCircle } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import axios from '../api/axios';
 
 const USER_REGEX = /^[a-zA-Z][a-zA-Z0-9-_]{3, 23}$/;
 const PWD_REGEX = /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%]).{8, 24}$/;
+
+const REGISTER_URL = '/register';
 
 const RegisterForm = () => {
   const userRef = useRef();
@@ -50,8 +53,30 @@ const RegisterForm = () => {
       setErrMsg('Invalid Entry');
       return;
     }
-    console.log(user, pwd)
-    setSuccess(true);
+
+    try {
+      const response = await axios.post(REGISTER_URL, JSON.stringify({ username: user, password: pwd }), {
+        headers: { 'Content-Type': 'application/json'},
+        withCredentials: true
+      });
+      // console.log(response.data);
+      // console.log(response.accessToken);
+      console.log(JSON.stringify(response));
+
+      setSuccess(true);
+      setUser('');
+      setPwd('')
+      setMatchPwd('');
+    } catch (err) {
+      if (!err?.response) {
+        setErrMsg('No Server Response');
+      } else if (err.response?.status === 409) {
+        setErrMsg('Username Taken');
+      } else {
+        setErrMsg('Registration Failed');
+      }
+      errRef.current.focus();
+    }
   }
 
   return (
