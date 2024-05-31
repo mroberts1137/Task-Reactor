8f00f5b1f1ca974604547551c717f249962f99de
mroberts1137 <mroberts1137@gmail.com>
2024-05-30 21:47:22 -0400

    improve list styling

--- src/components/list/List.js
+++ src/components/list/List.js
@@ -4,8 +4,14 @@ const List = ({ items, removeItem, displayKeys }) => {
   return (
     <div>
       <table>
+        <colgroup>
+          <col style={{ width: '15%' }} />
+          <col style={{ width: '30%' }} />
+          <col style={{ width: '20%' }} />
+        </colgroup>
         <thead>
           <tr className='list-item'>
+            <th className='list-item'></th>
             {Object.keys(displayKeys).map((key, index) => (
               <th key={index}>-{key}-</th>
             ))}
