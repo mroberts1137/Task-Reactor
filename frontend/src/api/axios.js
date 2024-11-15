a39cc99370ab14102264af3de03964c86bc4c7e3
mroberts1137 <mroberts1137@gmail.com>
2024-11-14 21:06:37 -0500

    update axios config

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -2,9 +2,9 @@ import axios from 'axios';
 
 const API_BASE_URL =
   process.env.NODE_ENV === 'production'
-    ? process.env.API_URL
-    : process.env.DEV_API_URL;
+    ? process.env.REACT_APP_API_URL
+    : process.env.REACT_APP_DEV_API_URL;
 
 export default axios.create({
-  baseURL: 'http://localhost:5000'
+  baseURL: API_BASE_URL
 });
