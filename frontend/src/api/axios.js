d74b8a2dc2977bcfb4f6dd8864f707619205cd1a
mroberts1137 <mroberts1137@gmail.com>
2024-11-14 20:31:34 -0500

    test production build

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,5 +1,10 @@
 import axios from 'axios';
 
+const API_BASE_URL =
+  process.env.NODE_ENV === 'production'
+    ? process.env.API_URL
+    : process.env.DEV_API_URL;
+
 export default axios.create({
   baseURL: 'http://localhost:5000'
 });
