a3480b94a9177c891083c50e19200c23b31c39f3
mroberts1137 <mroberts1137@gmail.com>
2024-11-20 00:35:58 -0500

    hard code API_BASE_URL in api_urls

--- frontend/src/api/api_urls.ts
+++ frontend/src/api/api_urls.ts
@@ -1,7 +1,7 @@
 export const API_BASE_URL =
   process.env.NODE_ENV === 'production'
-    ? process.env.REACT_APP_API_URL
-    : process.env.REACT_APP_DEV_API_URL;
+    ? 'https://task-reactor.onrender.com'
+    : 'http://localhost:5000';
 
 export const LOGIN_URL = API_BASE_URL + '/api/auth';
 export const LOGOUT_URL = API_BASE_URL + '/api/users/logout';
