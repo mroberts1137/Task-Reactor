6651c801b2519f5e99d0d33c63aa23e0cd5fe705
mroberts1137 <mroberts1137@gmail.com>
2024-12-12 01:13:07 -0500

    static server for running local build

---
+++ backend/serve-react-app.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+
+app.use(express.static(path.join(__dirname, '../frontend/build')));
+
+app.get('/*', function (req, res) {
+  res.sendFile(path.join(__dirname, '../frontend/build', 'index.html'));
+});
+
+app.listen(3001, () => {
+  console.log(`Server running at 'http://localhost:3001'`);
+});
